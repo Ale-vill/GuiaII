7a66efd323044368d25b1349ff0379ffc985edc4
Joseline <joseline.pineda@catolica.edu.sv>
2020-07-23 21:06:34 -0600

    punto numero 2 completado

---
+++ Guia2/Guia2/Controllers/PotenciaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guia2.Controllers
+{
+    public class PotenciaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(double numero, double potencia)
+        {
+            ViewData["resultado"] = Math.Pow(numero, potencia);
+            return View();
+        }
+    }
+}
