34728f32b2e26a794fccbfcb6c1efe3ce3afe0ef
Joseline <joseline.pineda@catolica.edu.sv>
2020-07-23 21:43:07 -0600

    calculo de promedio terminado

--- Guia2/Guia2/Controllers/PromedioController.cs
+++ Guia2/Guia2/Controllers/PromedioController.cs
@@ -17,7 +17,32 @@ namespace Guia2.Controllers
         [HttpPost]
         public IActionResult Index(double nota1, double nota2, double nota3)
         {
-            ViewData["Resultado"] = Math.Round(nota1 + nota2 + nota3 / 3, 2);
+           
+            var promedio = Math.Round((nota1 + nota2 + nota3) / 3, 2);
+            
+            string mensaje="";
+            if (promedio==10)
+            {
+                mensaje = "Felicidades su nota es:"+promedio;
+            }
+            if (promedio<10 && promedio>=7)
+            {
+                mensaje = "Aprobado su  nota es:"+promedio;
+            }
+            if (promedio < 10 && promedio >= 7)
+            {
+                mensaje = "Aprobado su  nota es:"+promedio;
+            }
+            if (promedio < 7 && promedio >4)
+            {
+                mensaje = "Reprobado su  nota es:"+promedio;
+            }
+            if (promedio <= 4)
+            {
+                mensaje = "Visite a su tutor"+promedio;
+            }
+
+            ViewData["resultado"] = mensaje;
 
             return View();
         }
