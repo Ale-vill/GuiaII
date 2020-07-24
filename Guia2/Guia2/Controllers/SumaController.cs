69681f3daee58c03e1ee1c5096cc55aa09e04d7b
Joseline <joseline.pineda@catolica.edu.sv>
2020-07-23 20:13:39 -0600

    commit inicial

---
+++ Guia2/Guia2/Controllers/SumaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guia2.Controllers
+{
+    public class SumaController : Controller
+    {
+        // GET: Suma
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
