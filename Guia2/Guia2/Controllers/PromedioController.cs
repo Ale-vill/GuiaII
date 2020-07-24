595cc9e0777dd8d2043c33e4446eafbc989ef341
Diego Henriquez <df.henriquez@hotmail.com>
2020-07-23 21:23:28 -0600

    Se ha creado la vista, para el controlador de promedios.

--- Guia2/Guia2/Controllers/PromedioController.cs
+++ Guia2/Guia2/Controllers/PromedioController.cs
@@ -8,11 +8,13 @@ namespace Guia2.Controllers
 {
     public class PromedioController : Controller
     {
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        [HttpPost]
         public IActionResult Index(double nota1, double nota2, double nota3)
         {
             ViewData["Resultado"] = Math.Round(nota1 + nota2 + nota3 / 3, 2);
