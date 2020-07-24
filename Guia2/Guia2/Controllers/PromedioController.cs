1f3200eb1e50cf93c7075878d81f2afc17ce72b4
Joseline <joseline.pineda@catolica.edu.sv>
2020-07-23 21:13:35 -0600

    iniciando el punto 3

---
+++ Guia2/Guia2/Controllers/PromedioController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Guia2.Controllers
+{
+    public class PromedioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Index(double nota1, double nota2, double nota3)
+        {
+            ViewData["Resultado"] = Math.Round(nota1 + nota2 + nota3 / 3, 2);
+
+            return View();
+        }
+    }
+}
